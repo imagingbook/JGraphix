de60b7225712f97e91dc5f08b386669e662f5f48
WilhelmBurger <wilhelm.burger@fh-hagenberg.at>
2017-11-18 15:28:49 +0100

    updated JavaDoc

---
+++ src/imagingbook/jgraphix/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Simple setup for performing non-interactive drawing
+ * operations in Java for educational use. 
+ * The implementation is based on Swing and AWT graphics. 
+ * Home repository: <a href="https://github.com/imagingbook/JGraphix">
+ * https://github.com/imagingbook/JGraphix</a>
+ * @author W. Burger
+ * @since 2017
+ */
+package imagingbook.jgraphix;
