ab2034a18a3a03fb646c2e571c6615c72b953b78
WilhelmBurger <wilhelm.burger@fh-hagenberg.at>
2017-11-18 13:19:28 +0100

    formatting

--- examples/JGraphix_Example_2.java
+++ examples/JGraphix_Example_2.java
@@ -15,16 +15,16 @@ public class JGraphix_Example_2 {
 	public static void main(String[] args) throws InterruptedException {
 		DrawFrame df = DrawFrame.create();
 		Graphics2D g = df.getGraphics2D();
-		
+
 		g.setColor(Color.BLUE);
-		
+
 		for (int i = 0; i < 100; i++) {
 			df.clear();
 			g.fillOval(80 + 3 * i, 40 + 2 * i, 30, 30 + i);
 			df.refresh();
-			Thread.sleep(25);	// wait for 25 ms
+			Thread.sleep(25); // wait for 25 ms
 		}
-		
+
 		System.out.println("done");
 	}
 
