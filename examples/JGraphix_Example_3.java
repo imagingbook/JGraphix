970dc3773fdd226472088e40e3d351235b0bf9c7
Wilhelm Burger <wilbur@ieee.org>
2017-11-21 17:20:30 +0100

    fixed

--- examples/JGraphix_Example_3.java
+++ examples/JGraphix_Example_3.java
@@ -17,28 +17,28 @@ public class JGraphix_Example_3 {
 	public static void main(String[] args) throws InterruptedException {
 		DrawFrame df = DrawFrame.create("example3", 800, 600);
 		Graphics2D g = df.getGraphics2D();
-		
+
 		int n = 50;
-		
+
 		g.setFont(new Font("Dialog", Font.PLAIN, 16));
 		g.drawString("Some Text", 10, 20);
-		
-	    double incX = (double) df.getW() / n;
-	    double incY = (double) df.getH() / n;
+
+		double incX = (double) df.getW() / n;
+		double incY = (double) df.getH() / n;
 
 		g.setStroke(new BasicStroke(1));
 		g.setColor(Color.BLACK);
 		g.drawOval(50, 50, 200, 100);
-		
-	   	for (int i = 1; i <= n; i++) {
-	    	int x1 = (int) Math.round(i * incX);
-	    	int y1 = 0;
-	    	int x2 = (int) Math.round(incX * (n - i));
-	    	int y2 = (int) Math.round(i * incY);
-	    	g.drawLine(x1, y1, x2, y2);
-	   	}
-	   	
-	   	df.refresh();
+
+		for (int i = 1; i <= n; i++) {
+			int x1 = (int) Math.round(i * incX);
+			int y1 = 0;
+			int x2 = (int) Math.round(incX * (n - i));
+			int y2 = (int) Math.round(i * incY);
+			g.drawLine(x1, y1, x2, y2);
+		}
+
+		df.refresh();
 	}
 
 }
